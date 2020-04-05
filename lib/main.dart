dcf8b0ae59776f97e4afc64701632514ec13c514
Gaurav Bagul <gauravsbagul@gmail.com>
2020-04-05 14:13:18 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: QuoteList()));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<String> quotes = [
+    'Quote one', 'Quote two', 'Quote three'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),
+      )
+    );
+  }
+}
